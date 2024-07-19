4ed22c20d16b2a0341d144700d90706d8d1e6882
polobal <yagiz.sanane62@gmail.com>
2024-07-19 10:34:12 +0300

    Add AdvanceService.scala

---
+++ src/main/scala/srdc/smartcds/cds/service/AdvanceService.scala
@@ -0,0 +1,33 @@
+package srdc.smartcds.cds.service
+
+import io.onfhir.cds.api.model.CdsResponse
+import io.onfhir.cds.service.{BaseCdsService, CdsServiceContext, CdsServiceRequest}
+import org.json4s.DefaultFormats
+import srdc.smartcds.cds.flow.AdvanceFlowExecution
+import srdc.smartcds.model.fhir.{Condition, MedicationStatement, Observation, Patient}
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class AdvanceService(cdsServiceContext: CdsServiceContext) extends BaseCdsService(cdsServiceContext) {
+  implicit val formats: DefaultFormats.type = DefaultFormats
+
+  override def executeCds(cdsServiceRequest: CdsServiceRequest)(implicit ex: ExecutionContext): Future[CdsResponse] = {
+    val patient = cdsServiceRequest.prefetches.get("patient").head.head.extract[Patient]
+    val AtrialFibrillation = cdsServiceRequest.prefetches.get("AtrialFibrillation").head.map(res => res.extract[Condition])
+    val DiabeticRetinopathy = cdsServiceRequest.prefetches.get("DiabeticNephropathy").head.map(res => res.extract[Observation])
+    val HypertensiveTreatment = cdsServiceRequest.prefetches.get("HypertensiveTreatment").head.map(res => res.extract[MedicationStatement])
+    val HbA1C = cdsServiceRequest.prefetches.get("HbA1C").head.map(res => res.extract[Observation])
+    val ACR = cdsServiceRequest.prefetches.get("ACR").head.map(res => res.extract[Observation])
+    val NonHDL = cdsServiceRequest.prefetches.get("NonHDL").head.map(res => res.extract[Observation])
+    val BP_SBP = cdsServiceRequest.prefetches.get("BP_SBP").head.map(res => res.extract[Observation])
+    val BP_DBP = cdsServiceRequest.prefetches.get("BP_DBP").head.map(res => res.extract[Observation])
+    val Type1Diabetes = cdsServiceRequest.prefetches.get("Type1Diabetes").head.map(res => res.extract[Condition])
+    val Type2Diabetes = cdsServiceRequest.prefetches.get("Type2Diabetes").head.map(res => res.extract[Condition])
+
+    val responseBuilder = createResponse(cdsServiceRequest)
+    Future{
+      AdvanceFlowExecution.executionFlow(patient, AtrialFibrillation, DiabeticRetinopathy, HypertensiveTreatment,
+        HbA1C, ACR, NonHDL, BP_SBP, BP_DBP, Type1Diabetes, Type2Diabetes,  responseBuilder).cdsResponse
+    }
+  }
+}
