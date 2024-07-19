255c65240761ea3cb56a408791e2fbc977cfb4a9
YarkınÖzcan <yrkozcn12@gmail.com>
2024-07-19 13:09:19 +0300

    fix the algotihm in qrisk3flow

--- src/main/scala/srdc/smartcds/cds/flow/QRisk3FlowExecution.scala
+++ src/main/scala/srdc/smartcds/cds/flow/QRisk3FlowExecution.scala
@@ -4,7 +4,7 @@ import io.onfhir.cds.model.CdsResponseBuilder
 import srdc.smartcds.model.fhir.{Condition, FamilyMemberHistory, MedicationStatement, Observation, Patient}
 import srdc.smartcds.util.{DateTimeUtil, FhirParseHelper, UnitConceptEnum}
 
-import scala.math.sqrt
+import scala.math.{exp, pow, sqrt}
 import scala.util.Try
 
 object QRisk3FlowExecution {
@@ -97,9 +97,12 @@ object QRisk3FlowExecution {
 
     var rati: Double = 4
 
+    var cholesterol: Double = 0
+    var hdl: Double = 0
+
     if (FhirParseHelper.checkObservationValuesExist(List(cholesterolObs, hdlObs))) {
-      val cholesterol = FhirParseHelper.getQuantityObservationValue(cholesterolObs, Option(UnitConceptEnum.CHOLESTEROL)).get
-      val hdl = FhirParseHelper.getQuantityObservationValue(hdlObs, Option(UnitConceptEnum.CHOLESTEROL)).get
+      cholesterol = FhirParseHelper.getQuantityObservationValue(cholesterolObs, Option(UnitConceptEnum.CHOLESTEROL)).get
+      hdl = FhirParseHelper.getQuantityObservationValue(hdlObs, Option(UnitConceptEnum.CHOLESTEROL)).get
       rati = cholesterol / hdl
     }
 
@@ -119,14 +122,28 @@ object QRisk3FlowExecution {
     val surv = 10
     val town = 0
     val ethrisk = 0
+    //val ethriskCode = Ethnicity.head.valueCodeableConcept.flatMap(_.coding.headOption.map(_.code)).getOrElse("0")
+
+/*    val ethrisk: Int = ethriskCode match {
+      case "0" => 1
+      case "1" => 2
+      case "2" => 3
+      case "3" => 4
+      case "4" => 5
+      case "5" => 6
+      case "6" => 7
+      case "7" => 8
+      case "8" => 9
+      case _ => 0
+    }*/
 
     if (patient.gender.contains("male")) {
       val patientScore = cvdMaleRaw(age, b_AF, b_atypicalantipsy, b_corticosteroids, b_impotence2, b_migraine, b_ra, b_renal, b_semi, b_sle, b_treatedhyp, b_type1, b_type2, bmi, ethrisk, fh_cvd, rati, sbp, sbpDeviation, smoke_cat, surv, town)
-      val healthyScore = cvdMaleRaw(age, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 25, 0, 0, 4, 125, 0, 0, surv, town)
+      val healthyScore = cvdMaleRaw(age, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 25, ethrisk, 0, 4, 125, 0, 0, surv, town)
       Some(patientScore,healthyScore)
     } else {
       val patientScore = cvdFemaleRaw(age, b_AF, b_atypicalantipsy, b_corticosteroids, b_migraine, b_ra, b_renal, b_semi, b_sle, b_treatedhyp, b_type1, b_type2, bmi, ethrisk, fh_cvd, rati, sbp, sbpDeviation, smoke_cat, surv, town)
-      val healthyScore = cvdFemaleRaw(age, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 25, 0, 0, 4, 125, 0, 0, surv, town)
+      val healthyScore = cvdFemaleRaw(age, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 25, ethrisk, 0, 4, 125, 0, 0, surv, town)
       Some(patientScore,healthyScore)
     }
   }
@@ -134,28 +151,12 @@ object QRisk3FlowExecution {
                   age: Int, b_AF: Int, b_atypicalantipsy: Int, b_corticosteroids: Int, b_impotence2: Int, b_migraine: Int, b_ra: Int, b_renal: Int, b_semi: Int, b_sle: Int, b_treatedhyp: Int, b_type1: Int, b_type2: Int, bmi: Double, ethrisk: Int, fh_cvd: Int, rati: Double, sbp: Double, sbps5: Double, smoke_cat: Int, surv: Int, town: Double
                 ): Double = {
 
-    val survivor = Array[Double](
-      0,
-      0,
-      0,
-      0,
-      0,
-      0,
-      0,
-      0,
-      0,
-      0,
-      0.977268040180206,
-      0,
-      0,
-      0,
-      0,
-      0
-    )
+    val survivor: Seq[Double] = Seq(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.977268040180206, 0, 0, 0, 0, 0)
+
 
     /* The conditional arrays */
 
-    val Iethrisk = Array[Double](
+    val Iethrisk: Seq[Double] = Seq(
       0,
       0,
       0.2771924876030827900000000,
@@ -168,7 +169,7 @@ object QRisk3FlowExecution {
       -0.2632134813474996700000000
     )
 
-    val Ismoke = Array[Double](
+    val Ismoke: Seq[Double] = Seq(
       0,
       0.1912822286338898300000000,
       0.5524158819264555200000000,
@@ -179,28 +180,31 @@ object QRisk3FlowExecution {
     /* Applying the fractional polynomial transforms */
     /* (which includes scaling)                      */
 
-    val dage = age.toDouble / 10
-    var age_1 = Math.pow(dage, -1)
-    var age_2 = Math.pow(dage, 3)
-    val dbmi = bmi / 10
-    var bmi_2 = Math.pow(dbmi, -2) * Math.log(dbmi)
-    var bmi_1 = Math.pow(dbmi, -2)
+    var dage: Double = age
+    dage = dage / 10
+    var age_1 = pow(dage,-1)
+    var age_2 = pow(dage,3)
+    var dbmi: Double = bmi
+    dbmi = dbmi / 10
+    var bmi_2 = pow(dbmi,-2)*scala.math.log(dbmi)
+    var bmi_1 = pow(dbmi,-2)
 
     /* Centring the continuous variables */
 
-    age_1 -= 0.234766781330109
-    age_2 -= 77.284080505371094
-    bmi_1 -= 0.149176135659218
-    bmi_2 -= 0.141913309693336
-    val ratiVal = rati - 4.300998687744141
-    val sbpVal = sbp - 128.571578979492190
-    val sbps5Val = sbps5 - 8.756621360778809
-    val townVal = town - 0.526304900646210
+    age_1 = age_1 - 0.234766781330109
+    age_2 = age_2 - 77.284080505371094
+    bmi_1 = bmi_1 - 0.149176135659218
+    bmi_2 = bmi_2 - 0.141913309693336
+    val _rati = rati - 4.300998687744141
+    val _sbp = sbp - 128.571578979492190
+    val _sbps5 = sbps5 - 8.756621360778809
+    val _town = town - 0.526304900646210
 
     /* Start of Sum */
-    var a = 0.0
+    var a: Double = 0
 
     /* The conditional sums */
+
     a += Iethrisk(ethrisk)
     a += Ismoke(smoke_cat)
 
@@ -210,10 +214,10 @@ object QRisk3FlowExecution {
     a += age_2 * 0.0022964880605765492000000
     a += bmi_1 * 2.4562776660536358000000000
     a += bmi_2 * -8.3011122314711354000000000
-    a += ratiVal * 0.1734019685632711100000000
-    a += sbpVal * 0.0129101265425533050000000
-    a += sbps5Val * 0.0102519142912904560000000
-    a += townVal * 0.0332682012772872950000000
+    a += _rati * 0.1734019685632711100000000
+    a += _sbp * 0.0129101265425533050000000
+    a += _sbps5 * 0.0102519142912904560000000
+    a += _town * 0.0332682012772872950000000
 
     /* Sum from boolean values */
 
@@ -237,40 +241,39 @@ object QRisk3FlowExecution {
     a += age_1 * (if (smoke_cat == 2) 0.7526867644750319100000000 else 0)
     a += age_1 * (if (smoke_cat == 3) 0.9931588755640579100000000 else 0)
     a += age_1 * (if (smoke_cat == 4) 2.1331163414389076000000000 else 0)
-    a += age_1 * (if (b_AF == 1) 3.4896675530623207000000000 else 0)
-    a += age_1 * (if (b_corticosteroids == 1) 1.1708133653489108000000000 else 0)
-    a += age_1 * (if (b_impotence2 == 1) -1.5064009857454310000000000 else 0)
-    a += age_1 * (if (b_migraine == 1) 2.3491159871402441000000000 else 0)
-    a += age_1 * (if (b_renal == 1) -0.5065671632722369400000000 else 0)
-    a += age_1 * (if (b_treatedhyp == 1) 6.5114581098532671000000000 else 0)
-    a += age_1 * (if (b_type1 == 1) 5.3379864878006531000000000 else 0)
-    a += age_1 * (if (b_type2 == 1) 3.6461817406221311000000000 else 0)
+    a += age_1 * b_AF * 3.4896675530623207000000000
+    a += age_1 * b_corticosteroids * 1.1708133653489108000000000
+    a += age_1 * b_impotence2 * -1.5064009857454310000000000
+    a += age_1 * b_migraine * 2.3491159871402441000000000
+    a += age_1 * b_renal * -0.5065671632722369400000000
+    a += age_1 * b_treatedhyp * 6.5114581098532671000000000
+    a += age_1 * b_type1 * 5.3379864878006531000000000
+    a += age_1 * b_type2 * 3.6461817406221311000000000
     a += age_1 * bmi_1 * 31.0049529560338860000000000
     a += age_1 * bmi_2 * -111.2915718439164300000000000
-    a += age_1 * (if (fh_cvd == 1) 2.7808628508531887000000000 else 0)
+    a += age_1 * fh_cvd * 2.7808628508531887000000000
     a += age_1 * sbp * 0.0188585244698658530000000
     a += age_1 * town * -0.1007554870063731000000000
     a += age_2 * (if (smoke_cat == 1) -0.0004985487027532612100000 else 0)
     a += age_2 * (if (smoke_cat == 2) -0.0007987563331738541400000 else 0)
     a += age_2 * (if (smoke_cat == 3) -0.0008370618426625129600000 else 0)
     a += age_2 * (if (smoke_cat == 4) -0.0007840031915563728900000 else 0)
-    a += age_2 * (if (b_AF == 1) -0.0003499560834063604900000 else 0)
-    a += age_2 * (if (b_corticosteroids == 1) -0.0002496045095297166000000 else 0)
-    a += age_2 * (if (b_impotence2 == 1) -0.0011058218441227373000000 else 0)
-    a += age_2 * (if (b_migraine == 1) 0.0001989644604147863100000 else 0)
-    a += age_2 * (if (b_renal == 1) -0.0018325930166498813000000 else 0)
-    a += age_2 * (if (b_treatedhyp == 1) 0.0006383805310416501300000 else 0)
-    a += age_2 * (if (b_type1 == 1) 0.0006409780808752897000000 else 0)
-    a += age_2 * (if (b_type2 == 1) -0.0002469569558886831500000 else 0)
+    a += age_2 * b_AF * -0.0003499560834063604900000
+    a += age_2 * b_corticosteroids * -0.0002496045095297166000000
+    a += age_2 * b_impotence2 * -0.0011058218441227373000000
+    a += age_2 * b_migraine * 0.0001989644604147863100000
+    a += age_2 * b_renal * -0.0018325930166498813000000
+    a += age_2 * b_treatedhyp * 0.0006383805310416501300000
+    a += age_2 * b_type1 * 0.0006409780808752897000000
+    a += age_2 * b_type2 * -0.0002469569558886831500000
     a += age_2 * bmi_1 * 0.0050380102356322029000000
     a += age_2 * bmi_2 * -0.0130744830025243190000000
-    a += age_2 * (if (fh_cvd == 1) -0.0002479180990739603700000 else 0)
+    a += age_2 * fh_cvd * -0.0002479180990739603700000
     a += age_2 * sbp * -0.0000127187419158845700000
     a += age_2 * town * -0.0000932996423232728880000
 
     /* Calculate the score itself */
-    val score = 100.0 * (1 - Math.pow(survivor(surv), Math.exp(a)))
-    score
+    100.0 * (1 - pow(survivor(surv), exp(a)))
   }
 
 
