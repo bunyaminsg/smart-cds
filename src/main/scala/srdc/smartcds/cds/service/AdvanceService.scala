adec637cfad504424592e5f46113033d29c4e22f
polobal <yagiz.sanane62@gmail.com>
2024-07-19 15:00:48 +0300

    Update AdvanceService to include cholesterol in more detail

--- src/main/scala/srdc/smartcds/cds/service/AdvanceService.scala
+++ src/main/scala/srdc/smartcds/cds/service/AdvanceService.scala
@@ -14,10 +14,12 @@ class AdvanceService(cdsServiceContext: CdsServiceContext) extends BaseCdsServic
   override def executeCds(cdsServiceRequest: CdsServiceRequest)(implicit ex: ExecutionContext): Future[CdsResponse] = {
     val patient = cdsServiceRequest.prefetches.get("patient").head.head.extract[Patient]
     val AtrialFibrillation = cdsServiceRequest.prefetches.get("AtrialFibrillation").head.map(res => res.extract[Condition])
-    val DiabeticRetinopathy = cdsServiceRequest.prefetches.get("DiabeticNephropathy").head.map(res => res.extract[Observation])
+    val DiabeticRetinopathy = cdsServiceRequest.prefetches.get("DiabeticRetinopathy").head.map(res => res.extract[Observation])
     val HypertensiveTreatment = cdsServiceRequest.prefetches.get("HypertensiveTreatment").head.map(res => res.extract[MedicationStatement])
     val HbA1C = cdsServiceRequest.prefetches.get("HbA1C").head.map(res => res.extract[Observation])
     val ACR = cdsServiceRequest.prefetches.get("ACR").head.map(res => res.extract[Observation])
+    val TotalCholesterol = cdsServiceRequest.prefetches.get("TotalCholesterol").head.map(res => res.extract[Observation])
+    val HDL = cdsServiceRequest.prefetches.get("HDL").head.map(res => res.extract[Observation])
     val NonHDL = cdsServiceRequest.prefetches.get("NonHDL").head.map(res => res.extract[Observation])
     val BP_SBP = cdsServiceRequest.prefetches.get("BP_SBP").head.map(res => res.extract[Observation])
     val BP_DBP = cdsServiceRequest.prefetches.get("BP_DBP").head.map(res => res.extract[Observation])
@@ -27,7 +29,7 @@ class AdvanceService(cdsServiceContext: CdsServiceContext) extends BaseCdsServic
     val responseBuilder = createResponse(cdsServiceRequest)
     Future{
       AdvanceFlowExecution.executionFlow(patient, AtrialFibrillation, DiabeticRetinopathy, HypertensiveTreatment,
-        HbA1C, ACR, NonHDL, BP_SBP, BP_DBP, Type1Diabetes, Type2Diabetes,  responseBuilder).cdsResponse
+        HbA1C, ACR, TotalCholesterol, HDL, NonHDL, BP_SBP, BP_DBP, Type1Diabetes, Type2Diabetes,  responseBuilder).cdsResponse
     }
   }
 }
